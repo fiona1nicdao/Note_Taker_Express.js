c0a52893e9c9b088bf6ea59e3a317fb62364d449
FionaN <fiona.nicdao@northwestern.edu>
2021-12-12 23:31:24 -0600

    trial3

--- server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
-const api = require('./public/assets/js/index');
-
-const PORT = process.env.PORT || 3001
-
-const app = express();
