8d3762fa48c2ec6188075ebc1506af91f6ad7f91
FionaN <fiona.nicdao@northwestern.edu>
2021-12-14 22:34:41 -0600

    clean up server.js

--- server.js
+++ server.js
@@ -3,19 +3,18 @@ const path = require('path');
 const fs = require('fs');
 const uuid = require('./public/assets/js/uuid');
 const database = require('mime-db');
-// const util = require('util');
 
 const PORT = process.env.PORT || 3001
 
 const app = express();
-// middleware?
+
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
 app.use(express.static('public'));
 
 
-// GET Route for homepage (index.html) (why *?)
+// GET Route for homepage (index.html)
 app.get('/',(req, res) =>
     res.sendFile(path.join(__dirname,'/public/index.html'))
 ); 
@@ -53,7 +52,6 @@ app.post('/api/notes',(req,res)=>{
     } else {
         res.status(500).json('Error in posting review');
     }
-    // let addNote = req.body;
 })
 
 
